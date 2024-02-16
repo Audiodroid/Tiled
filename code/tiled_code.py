6380acc3ff1e29fc800038f9a75f52851972fad9
audiodroid <marcel.roth76@gmail.com>
2024-02-16 21:24:30 +0000

    tidy and improve

--- code/tiled_code.py
+++ code/tiled_code.py
@@ -2,6 +2,9 @@ import pygame
 from pytmx.util_pygame import load_pygame
 import tools
 
+TILE_WIDTH = 128
+TILE_HEIGHT = 128
+
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -10,42 +13,13 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
 
 
-def update(screen, surface, pos, sprite_group, mover=None, zoomer=None):
+def setup(tmx_data, sprite_group):
 
-    sprite_group.draw(surface)
-    screen.fill('black')
-
-    pos = mover.get_new_pos(pos)
-    zoom_size = zoomer.get_zoom_size()
-    surface = pygame.transform.scale(surface, zoom_size)
-    screen.blit(surface, pos)
-
-    pygame.display.flip()
-    pygame.display.update()
-
-
-def main():
-    pygame.init()
-
-    x = 1280
-    y = 720
-    screen_size = (x, y)
-    screen = pygame.display.set_mode(screen_size)
-
-    x2 = 4480
-    y2 = 4096
-    surface_size = (x2, y2)
-    surface = pygame.surface.Surface(surface_size)
-
-    tmx_data = load_pygame('../data/tmx/basic.tmx')
-    sprite_group = pygame.sprite.Group()
-
-    # cycle through all layers
     for layer in tmx_data.visible_layers:
         # if layer.name in ('Floor', 'Plants and rocks', 'Pipes')
         if hasattr(layer, 'data'):
             for x, y, surf in layer.tiles():
-                pos = (x * 128, y * 128)
+                pos = (x * TILE_WIDTH, y * TILE_HEIGHT)
                 Tile(pos=pos, surf=surf, groups=sprite_group)
 
     for obj in tmx_data.objects:
@@ -53,28 +27,32 @@ def main():
         if obj.type in ('Building', 'Vegetation'):
             Tile(pos=pos, surf=obj.image, groups=sprite_group)
 
-    for obj in tmx_data.objects:
-        if obj.type == 'Shape':
-            if obj.name == 'Marker':
-                pygame.draw.circle(screen, 'red', (obj.x, obj.y), 5)
-            if obj.name == 'Rectangle':
-                rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
-                pygame.draw.rect(screen, 'yellow', rect)
 
-            if obj.name == 'Ellipse':
-                rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
-                pygame.draw.ellipse(screen, 'blue', rect)
+def update(screen, surface, pos, sprite_group, mover=None, zoomer=None):
 
-            if obj.name == 'Polygon':
-                points = [(point.x, point.y) for point in obj.points]
-                pygame.draw.polygon(screen, 'green', points)
+    sprite_group.draw(surface)
+    screen.fill('black')
+
+    zoom_size = zoomer.get_zoom_size()
+    zoom = zoomer.get_zoom()
+    pos = mover.get_new_pos(pos, zoom)
+    surface = pygame.transform.scale(surface, zoom_size)
+    screen.blit(surface, pos)
+
+    pygame.display.flip()
+    pygame.display.update()
+
+
+def run(screen, surface, sprite_group):
 
     clock = pygame.time.Clock()
 
     mover = tools.Mover(screen, surface)
     zoomer = tools.Zoomer(screen)
     pos = [0, 0]
+
     update(screen, surface, pos, sprite_group, mover=mover, zoomer=zoomer)
+
     quit_game = False
     while not quit_game:
 
@@ -85,6 +63,29 @@ def main():
             update(screen, surface, pos, sprite_group, mover=mover, zoomer=zoomer)
 
         clock.tick(60)
+
+
+def main():
+    pygame.init()
+
+    x = 640
+    y = 360
+    screen_size = (x, y)
+    screen = pygame.display.set_mode(screen_size)
+
+    tmx_data = load_pygame('../data/tmx/basic.tmx')
+
+    x2 = tmx_data.tilewidth * tmx_data.width
+    y2 = tmx_data.tileheight * tmx_data.height
+
+    surface_size = (x2, y2)
+    surface = pygame.surface.Surface(surface_size)
+
+    sprite_group = pygame.sprite.Group()
+
+    setup(tmx_data, sprite_group)
+
+    run(screen, surface, sprite_group)
 
 
 if __name__ == "__main__":
--- code/tools.py
+++ code/tools.py
@@ -1,11 +1,14 @@
 import pygame
 
 
-def key_is_move_key(event):
-    return event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s
-
-
 class Mover:
+
+    MOVE_KEYS = [
+        pygame.K_LEFT,
+        pygame.K_RIGHT,
+        pygame.K_UP,
+        pygame.K_DOWN
+    ]
 
     def __init__(self, screen, surface):
         self.screen = screen
@@ -17,7 +20,8 @@ class Mover:
     def check_for_move_request(self):
 
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and key_is_move_key(event):
+
+            if (event.type == pygame.KEYDOWN) and (event.key in self.MOVE_KEYS):
                 self.move_key_is_down = True
                 self.key = event.key
             elif event.type == pygame.KEYUP:
@@ -26,137 +30,44 @@ class Mover:
 
         return self.move_key_is_down
 
-    def new_pos(self, pos):
+    def new_pos(self, pos, zoom):
 
-        max_x = self.surface.get_size()[0] - self.screen.get_size()[0]
-        max_y = self.surface.get_size()[1] - self.screen.get_size()[1]
+        w = self.screen.get_size()[0]
+        h = self.screen.get_size()[1]
 
-        if self.key == pygame.K_a:
+        if self.key == pygame.K_LEFT:
             pos[0] = min(pos[0] + 15, 0)
 
-        elif self.key == pygame.K_d:
-            pos[0] = max(pos[0] - 15, -max_x)
+        elif self.key == pygame.K_RIGHT:
+            pos[0] = max(pos[0] - 15, -(w / zoom - w))  # TODO makes no sense
 
-        elif self.key == pygame.K_w:
+        elif self.key == pygame.K_UP:
             pos[1] = min(pos[1] + 15, 0)
 
-        elif self.key == pygame.K_s:
-            pos[1] = max(pos[1] - 15, -max_y)
+        elif self.key == pygame.K_DOWN:
+            pos[1] = max(pos[1] - 15, -(h / zoom - h))  # TODO makes no sense
 
         return pos
 
-    def get_new_pos(self, pos):
+    def get_new_pos(self, pos, zoom):
 
         if not self.move_key_is_down:
             return pos
 
-        return self.new_pos(pos)
+        return self.new_pos(pos, zoom)
 
 
 class Zoomer:
-
     """ https://stackoverflow.com/questions/56407891/pygame-transform-scale-does-not-work-on-the-game-surface """
 
     def __init__(self, screen):
         self.screen = screen
-        self.zoom = 0.5
+        self.zoom = 0.25
+
+    def get_zoom(self):
+        return self.zoom
 
     def get_zoom_size(self):
-        """zoom=1 shows the full surface, 0.5 shows the top quarter!?"""
+        """zoom=1 shows the full surface, 0.5 shows the top quarter,..."""
         wnd_w, wnd_h = self.screen.get_size()
         return round(wnd_w / self.zoom), round(wnd_h / self.zoom)
-
-
-class Scroller:
-
-    def __init__(self, display, surface, scroll_y):
-        self.scroll_y = scroll_y
-        self.display = display
-        self.surface = surface
-
-    def handle_scroll(self):
-
-        MOUSE_WHEEL_SCROLL_UP = 4
-        MOUSE_WHEEL_SCROLL_DOWN = 5
-
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == MOUSE_WHEEL_SCROLL_UP:
-                    self.scroll_y = min(self.scroll_y + 15, 0)
-                if event.button == MOUSE_WHEEL_SCROLL_DOWN:
-                    self.scroll_y = max(self.scroll_y - 15, -1280)
-
-        self.display.blit(self.surface, (0, self.scroll_y))
-
-
-class Printer:
-
-    def print_pygame(self, tmx_data):
-
-        # get layers
-        print(tmx_data.layers) # get all layers
-        for layer in tmx_data.visible_layers: # get visible layers
-            print(layer)
-
-        print(tmx_data.layernames) # get all layer names as dict
-
-        print(tmx_data.get_layer_by_name('Floor')) # get one layer by name
-
-        for obj in tmx_data.objectgroups: # get object layers
-            print(obj)
-
-        # get tiles
-        layer = tmx_data.get_layer_by_name('Floor')
-        for x,y,surf in layer.tiles(): # get all the information
-            print(x * 128)
-            print(y * 128)
-            print(surf)
-
-        print(layer.data)
-
-        print(layer.name)
-        print(layer.id)
-
-        # get objects
-        object_layer = tmx_data.get_layer_by_name('Objects')
-        for obj in object_layer:
-            # print(obj.x)
-            # print(obj.y)
-            # print(obj.image)
-            if obj.type == 'Shape':
-                if obj.name == 'Marker':
-                    print(obj.x)
-                    print(obj.y)
-                if obj.name == 'Rectangle':
-                    print(obj.x)
-                    print(obj.y)
-                    print(obj.width)
-                    print(obj.height)
-                    print(obj.as_points)
-
-                if obj.name == 'Ellipse':
-                    print(dir(obj))
-
-                if obj.name == 'Polygon':
-                    print(obj.as_points)
-                    print(obj.points)
-
-        for obj in tmx_data.objects:
-            print(obj)
-
-        for obj in tmx_data.objects:
-            pos = obj.x, obj.y
-            if obj.type == 'Shape':
-                if obj.name == 'Marker':
-                    pygame.draw.circle(self.screen, 'red', (obj.x, obj.y), 5)
-                if obj.name == 'Rectangle':
-                    rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
-                    pygame.draw.rect(self.screen, 'yellow', rect)
-
-                if obj.name == 'Ellipse':
-                    rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
-                    pygame.draw.ellipse(self.screen, 'blue', rect)
-
-                if obj.name == 'Polygon':
-                    points = [(point.x, point.y) for point in obj.points]
-                    pygame.draw.polygon(self.screen, 'green', points)
